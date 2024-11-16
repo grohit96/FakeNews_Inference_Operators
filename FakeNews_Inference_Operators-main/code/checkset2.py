903c41c1a24ed792cdff092990dac80a3f113974
G. Rohit <grohit96@gmail.com>
2024-11-16 18:35:25 -0500

    Update checkset2.py

--- FakeNews_Inference_Operators-main/code/checkset2.py
+++ FakeNews_Inference_Operators-main/code/checkset2.py
@@ -1,3 +1,3 @@
 import numpy as np
 test_set = np.load("/scratch/rganesh5/PaperGraph_release_connected/test_set_1.npy", allow_pickle=True)
-print(type(test_set), test_set[:5])  # Print type and first 5 entries
+print(type(test_set), test_set[:5]) 
